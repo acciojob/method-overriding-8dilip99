2a9be3f6b75d474167c815c14509bf0c7547d7f4
Dilip Upadhyay <dilipupadhyayfea@gmail.com>
2025-09-19 19:53:13 +0530

    try

--- src/main/java/com/driver/A.java
+++ src/main/java/com/driver/A.java
@@ -1,15 +1,8 @@
 package com.driver;
 
- class A {
-     String meth() {
+public class A {
+   public   String meth() {
         return "Invoking method from class A";
     }
  }
 
-class B extends  A{
-    @Override
-    String meth() {
-        return "Method is Overridden in Extended class B";
-    }
-
-}
