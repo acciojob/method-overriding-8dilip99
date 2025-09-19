53765d827b378e8f2bfc8c28821ec99a5d671349
Dilip Upadhyay <dilipupadhyayfea@gmail.com>
2025-09-19 20:18:50 +0530

    letsee

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,11 +1,26 @@
 package com.driver;
 
 public class Main {
+    public static class A {
+        public   String meth() {
+            return  "Invoking method from class A";
+        }
+    }
+
+    public static class B extends com.driver.A {
+        @Override
+        public   String meth() {
+            return  "Method is overridden in Extendend class B";
+        }
+
+    }
+
     public static void main(String[] args) {
         B obj= new B();
+        obj.meth(); //Automatically call the function class A meth
 
-
-
-        System.out.println(obj.meth());
+//        System.out.println(obj.meth());
+        B obj1=new B();
+        obj1.meth();
     }
 }
